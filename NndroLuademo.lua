c3a89d81b4db5c334ab60207a5ef03ce6d6cf741
异世界6666 <3353738685@qq.com>
2022-07-28 09:25:59 +0000

    update NndroLuademo.lua.

--- NndroLuademo.lua
+++ NndroLuademo.lua
@@ -1,5 +1,5 @@
 
-import "bmob"
-id="b30d60f353c96b89114989c65563a3c9"
-key="fdc0d41bd119a25d41db625e25ad0342"
- bmob=bmob(id,key)
+--import "bmob"
+--id="b30d60f353c96b89114989c65563a3c9"
+--key="fdc0d41bd119a25d41db625e25ad0342"
+-- bmob=bmob(id,key)

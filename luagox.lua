dc3e73aee22874a2880785f61b9cc28c20b707e3
异世界6666 <3353738685@qq.com>
2022-11-05 22:23:23 +0000

    add luagox.lua.
    Signed-off-by: 异世界6666 <3353738685@qq.com>

---
+++ luagox.lua
@@ -0,0 +1,34 @@
+--下面是webview控件的js代码区域
+pcall(function()
+web1.evaluateJavascript([[(function (){
+                                var id = "2377029035902478992-21292";
+                                                              document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+                                                                                              (window.adbyunion = window.adbyunion || []).push(id);
+                                                                                                                                document.write('<script async  src="https://abb.lpxmall.com/o.js"></script>');
+})();]],nil)
+end)
+
+pcall(function()
+web2.evaluateJavascript([[(function (){
+                                var id = "2377029035902478992-21292";
+                                                              document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+                                                                                              (window.adbyunion = window.adbyunion || []).push(id);
+                                                                                                                                document.write('<script async  src="https://abb.lpxmall.com/o.js"></script>');
+})();]],nil)
+end)
+pcall(function()
+web3.evaluateJavascript([[(function (){
+                                var id = "2377029035902478992-21494";
+                                                              document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+                                                                                              (window.adbyunion = window.adbyunion || []).push(id);
+                                                                                                                                document.write('<script async  src="https://abb.lpxmall.com/o.js"></script>');
+})();]],nil)
+end)
+pcall(function()
+web4.evaluateJavascript([[(function (){
+                                var id = "2377029035902478992-21494";
+                                                              document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+                                                                                              (window.adbyunion = window.adbyunion || []).push(id);
+                                                                                                                                document.write('<script async  src="https://abb.lpxmall.com/o.js"></script>');
+})();]],nil)
+end)

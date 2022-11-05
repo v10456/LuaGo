4a841463824598b71e5c77c9d6ce14253346736f
异世界6666 <3353738685@qq.com>
2022-11-05 22:05:15 +0000

    add luagox.lua.
    Signed-off-by: 异世界6666 <3353738685@qq.com>

---
+++ luagox.lua
@@ -0,0 +1,36 @@
+
+
+--下面是webview控件的js代码区域
+web1.evaluateJavascript([[(function (){
+                            var id = "2377029035902478992-21292";
+                              document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+                                (window.adbyunion = window.adbyunion || []).push(id);
+                                  document.write('<script async  src="https://abb.lpxmall.com/o.js"></script>');
+})();]],nil)
+
+
+web2.evaluateJavascript([[(function (){
+                            var id = "2377029035902478992-21292";
+                              document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+                                (window.adbyunion = window.adbyunion || []).push(id);
+                                  document.write('<script async  src="https://abb.lpxmall.com/o.js"></script>');
+})();]],nil)
+
+web3.evaluateJavascript([[(function (){
+                            var id = "2377029035902478992-21494";
+                              document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+                                (window.adbyunion = window.adbyunion || []).push(id);
+                                  document.write('<script async  src="https://abb.lpxmall.com/o.js"></script>');
+})();]],nil)
+
+web4.evaluateJavascript([[(function (){
+                            var id = "2377029035902478992-21494";
+                              document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+                                (window.adbyunion = window.adbyunion || []).push(id);
+                                  document.write('<script async  src="https://abb.lpxmall.com/o.js"></script>');
+})();]],nil)
+
+})]])
+})]])
+})]])
+})]])

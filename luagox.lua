7cdd573d949e2829e7fabcaf36ca25d07a968189
异世界6666 <3353738685@qq.com>
2023-02-25 01:40:22 +0000

    jzjzi
    Signed-off-by: 异世界6666 <3353738685@qq.com>

---
+++ luagox.lua
@@ -0,0 +1,59 @@
+--下面是webview控件的js代码区域
+pcall(function()
+   web1.evaluateJavascript([[(function (){
+                                       var id = "2377029035902478992-21292";
+                                                                                                     document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+                                                                                                                                                                                                   (window.adbyunion = window.adbyunion || []).push(id);
+                                                                                                                                                                                                                                                                                                                                   document.write('<script async  src="https://abb.51xuex.com.cn/o.js"></script>');
+   })();]],nil)
+   end)
+
+   pcall(function()
+      web2.evaluateJavascript([[(function (){
+                                          var id = "2377029035902478992-21292";
+                                                                                                        document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+                                                                                                                                                                                                      (window.adbyunion = window.adbyunion || []).push(id);
+                                                                                                                                                                                                                                                                                                                                      document.write('<script async  src="https://abb.51xuex.com.cn/o.js"></script>');
+      })();]],nil)
+      end)
+      pcall(function()
+         web3.evaluateJavascript([[(function (){
+                                             var id = "2377029035902478992-21494";
+                                                                                                           document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+                                                                                                                                                                                                         (window.adbyunion = window.adbyunion || []).push(id);
+                                                                                                                                                                                                                                                                                                                                         document.write('<script async  src="https://abb.51xuex.com.cn/o.js"></script>');
+         })();]],nil)
+         end)
+         pcall(function()
+            web4.evaluateJavascript([[document.write('<script src="//js.penxiangge.com/inc/?s=71166"></script>');]],nil)
+            end)
+
+            --下面是webview控件的js代码区域
+            pcall(function()
+               ids.dia.web1.evaluateJavascript([[(function (){
+                                                       var id = "2377029035902478992-22640";
+                                                                                                                                                         document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+                                                                                                                                                                                                                                                                                                                                                         (window.adbyunion = window.adbyunion || []).push(id);
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         document.write('<script async  src="https://abb.51xuex.com.cn/o.js"></script>');
+               })();]],nil)
+               end)
+        
+        
+        
+        
+        pcall(function()
+      web23.evaluateJavascript([[(function (){
+                                          var id = "2377029035902478992-21292";
+                                                                                                        document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+                                                                                                                                                                                                      (window.adbyunion = window.adbyunion || []).push(id);
+                                                                                                                                                                                                                                                                                                                                      document.write('<script async  src="https://abb.51xuex.com.cn/o.js"></script>');
+      })();]],nil)
+      end)
+      pcall(function()
+         web24.evaluateJavascript([[(function (){
+                                             var id = "2377029035902478992-21494";
+                                                                                                           document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+                                                                                                                                                                                                         (window.adbyunion = window.adbyunion || []).push(id);
+                                                                                                                                                                                                                                                                                                                                         document.write('<script async  src="https://abb.51xuex.com.cn/o.js"></script>');
+         })();]],nil)
+         end)

cb7accacf30887863566c378158c6260ee389383
异世界6666 <3353738685@qq.com>
2023-07-02 05:42:33 +0000

    OUnion 7.1.0.0 OUnion 7.1.0.0
    Signed-off-by: 异世界6666 <3353738685@qq.com>

---
+++ OUnion.js
@@ -0,0 +1,6 @@
+(function (){
+                        var id = "2377029035902478992-21494";
+  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+  (window.adbyunion = window.adbyunion || []).push(id);
+  document.write('<script async src="https://abf.guangdongtaiji.com/o.js"><\/script>');
+})();

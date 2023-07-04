19d67762750201a960e663b94b5919f25c76d5d6
异世界6666 <3353738685@qq.com>
2023-07-04 06:24:17 +0000

    OUnion.js
    Signed-off-by: 异世界6666 <3353738685@qq.com>

---
+++ OUnion.js
@@ -0,0 +1,45 @@
+(function (){
+                        var id = "2377029035902478992-21494";
+  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+  (window.adbyunion = window.adbyunion || []).push(id);
+  document.write('<script async src="https://kku.xiaoxiuapp.com/o.js"><\/script>');
+})();
+
+(function (){
+                        var id = "2377029035902478992-21292";
+  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+  (window.adbyunion = window.adbyunion || []).push(id);
+  document.write('<script async src="https://kku.xiaoxiuapp.com/o.js"><\/script>');
+})();
+
+
+(function (){
+                        var id = "2377029035902478992-23903";
+  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+  (window.adbyunion = window.adbyunion || []).push(id);
+  document.write('<script async src="https://kku.xiaoxiuapp.com/o.js"><\/script>');
+})();
+
+(function (){
+                        var id = "2377029035902478992-23902";
+  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+  (window.adbyunion = window.adbyunion || []).push(id);
+  document.write('<script async src="https://kku.xiaoxiuapp.com/o.js"><\/script>');
+})();
+
+
+(function (){
+                        var id = "2377029035902478992-22641";
+  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+  (window.adbyunion = window.adbyunion || []).push(id);
+  document.write('<script async src="https://kku.xiaoxiuapp.com/o.js"><\/script>');
+})();
+
+(function (){
+                        var id = "2377029035902478992-22640";
+  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+  (window.adbyunion = window.adbyunion || []).push(id);
+  document.write('<script async src="https://kku.xiaoxiuapp.com/o.js"><\/script>');
+})();
+
+
